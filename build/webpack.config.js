5937bf8a5616f5867f0a39ef8f024521d63565c3
dingsheng <dingduoduo0916@gmail.com>
2019-04-11 21:57:16 +0800

    切换到vscode

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 module.exports = {
   entry: {
     index: path.join(__dirname, '../lib/index.tsx')
